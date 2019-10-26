d46d83e2ac3d43344392073a7b97730e27b20380
Nick Vasic <nick.vasic@certussolutions.com>
2019-10-26 19:55:49 +1100

    Now for the guest patch finally

--- android/src/main/java/expo/modules/appauth/AppAuthModule.java
+++ android/src/main/java/expo/modules/appauth/AppAuthModule.java
@@ -304,7 +304,7 @@ public class AppAuthModule extends ExportedModule {
     Intent postAuthIntent = new Intent(activity, AppAuthBrowserActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
 
     ConstantsInterface constantsService = mModuleRegistry.getModule(ConstantsInterface.class);
-    if (!"standalone".equals(constantsService.getAppOwnership())) {
+    if (!constantsService.getAppOwnership().matches("standalone|guest")) {
       if (!constantsService.getConstants().containsKey(AppAuthConstants.MANIFEST_URL)) {
         mAuthTask.reject(AppAuthConstants.Error.DEFAULT, "Missing " + AppAuthConstants.MANIFEST_URL + " in the experience Constants");
         return;
